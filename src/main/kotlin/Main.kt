10ab26ed10e61cdb408cee32a73cc8da7734b2c3
farud <farudbox@gmail.com>
2024-11-10 17:05:48 +0300

    KTB-03 adding correctAnswersCount property and improving data handling in Word

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,26 @@ import java.io.File
 
 fun main() {
 
+    val dictionary = mutableMapOf<String, Word>()
+
     val file = File("words.txt")
-    val lines = file.readLines()
+    val lines: List<String> = file.readLines()
 
     for (line in lines) {
-        println(line)
+        val parts = line.split("|")
+
+        if (parts.size >= 2) {
+            val word = parts[0].trim()
+            val translate = parts[1].trim()
+            val correctAnswersCount = if (parts.size == 3) parts[2].trim().toIntOrNull() ?: 0 else 0
+
+            dictionary[word] = Word(word, translate, correctAnswersCount)
+        } else {
+            println("Некорректный формат строки: $line")
+        }
+    }
+
+    for ((_, word) in dictionary) {
+        println(word)
     }
 }
